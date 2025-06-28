c749b8b7d102c62065e04fe633664499d9281c67
Sandeep M B <sandeepm@canarys.com>
2025-06-28 12:22:50 +0530

    All issues fixed

--- src/main/java/com/example/RealEstateBackend/config/MongoConfig.java
+++ src/main/java/com/example/RealEstateBackend/config/MongoConfig.java
@@ -3,25 +3,16 @@ package com.example.RealEstateBackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Optional;
 
 @Configuration
 @EnableMongoAuditing
-@EnableTransactionManagement
 public class MongoConfig {
     
     @Bean
     public AuditorAware<String> auditorProvider() {
         return () -> Optional.of("system");
-    }
-    
-    @Bean
-    public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
-        return new MongoTransactionManager(dbFactory);
     }
 }
